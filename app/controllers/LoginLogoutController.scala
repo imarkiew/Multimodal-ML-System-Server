cb2c1c67ac4e83e7fee9231b70612d8e7f6244d9
Igor Markiewicz <igomar067@gmail.com>
2019-10-18 13:05:35 +0200

    Added table with (dummy) results

--- app/controllers/LoginLogoutController.scala
+++ app/controllers/LoginLogoutController.scala
@@ -2,9 +2,10 @@ package controllers
 
 import models.dao.UserDao
 import javax.inject.{Inject, Singleton}
-import models.dto.User
+import models.dto.{Examination, User}
 import models.utilities.UserCredentialsUtilities.{getUserCredentialsFromRequestBody, performAction}
 import play.api.mvc.{AbstractController, ControllerComponents}
+
 import scala.concurrent.{ExecutionContext, Future}
 import org.mindrot.jbcrypt.BCrypt.checkpw
 
@@ -15,6 +16,11 @@ class LoginLogoutController @Inject()(userDao: UserDao, controllerComponents: Co
 
   import models.implicits.UserImplicits._
   import models.implicits.ExaminationsImplicits._
+
+  val exams = Seq(
+    Examination(11, "skin-lesions", "Patient 1", "2019-10-11 12:28:37", Map("akiec" -> 57, "ner" -> 20, "true" -> 23)),
+    Examination(5, "breast-cancer", "Patient 3", "2019-10-11 12:30:37", Map("mal" -> 60, "bem" -> 40))
+  )
 
   def index = Action.async { implicit request =>
 
@@ -30,13 +36,13 @@ class LoginLogoutController @Inject()(userDao: UserDao, controllerComponents: Co
   def validate = Action.async { implicit request =>
 
     performAction { _ =>
-      Future(Ok(views.html.cockpit(request.session.get("username").get)))
+      Future(Ok(views.html.cockpit(request.session.get("username").get, exams)))
     }({
       val requestVals = getUserCredentialsFromRequestBody(request)
       userDao
         .getUser(requestVals.username.get)
         .map {
-          case Some(user: User) if checkpw(requestVals.password.get, user.passwordHash) => Ok(views.html.cockpit(user.username))
+          case Some(user: User) if checkpw(requestVals.password.get, user.passwordHash) => Ok(views.html.cockpit(user.username, exams))
             .withSession("username" -> user.username, "Csrf-Token" -> play.filters.csrf.CSRF.getToken.get.value)
           case _ => Ok(views.html.login(Some("Invalid username or password")))
         }
@@ -46,7 +52,7 @@ class LoginLogoutController @Inject()(userDao: UserDao, controllerComponents: Co
 
   def cockpit = Action.async { implicit request =>
     performAction { _ =>
-      Future(Ok(views.html.cockpit(request.session.get("username").get)))
+      Future(Ok(views.html.cockpit(request.session.get("username").get, exams)))
     }(Future(Ok(views.html.login(None))))
   }
 
