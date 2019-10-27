9d01788b93396453e47c1ffdf4f48c3eefa09d76
Igor Markiewicz <igomar067@gmail.com>
2019-10-27 15:16:34 +0100

    Added sorting of examinations by percentage

--- app/models/implicits/ExaminationsImplicits.scala
+++ app/models/implicits/ExaminationsImplicits.scala
@@ -2,6 +2,7 @@ package models.implicits
 
 import models.utilities.CustomConfig.customConfig
 import play.api.libs.json._
+import scala.collection.immutable.ListMap
 
 
 object ExaminationsImplicits {
@@ -9,7 +10,12 @@ object ExaminationsImplicits {
   implicit val examinationNames: Seq[String] = customConfig.examinations
 
   implicit def examinationResultsStringAsJsonToMap(jsonString: String): Map[String, Double] =
-    Json.parse(jsonString).validate[Map[String, Double]].get
+    ListMap(Json
+      .parse(jsonString)
+      .validate[Map[String, Double]]
+      .get
+      .toSeq
+      .sortBy(_._2)(Ordering[Double].reverse):_*)
 
   implicit def examinationResultsMapToStringAsJson(results: Map[String, Double]): String = Json.toJson(results).toString
 
